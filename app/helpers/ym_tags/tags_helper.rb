36da4ed4ccf4fadfb624c3a0c15fb2319f0f3160
Matt Atkins <matt@yoomee.com>
2012-06-21 17:10:57 +0100

    Tag options

--- app/helpers/ym_tags/tags_helper.rb
+++ app/helpers/ym_tags/tags_helper.rb
@@ -4,12 +4,15 @@ module YmTags::TagsHelper
     tag_object = tag_object.to_tag
     active = options.delete(:active)
     options[:class] = "tag #{options[:class]}".strip
+    label_options = options.delete(:label_options) || {}
     link_to(tag_object, options) do
-      tag_label(tag_object, :active => active)
+      tag_label(tag_object, label_options.merge(:active => active))
     end
   end
   
   def tag_label(tag_or_html, options = {})
+    options[:method] ||= :to_s
+    tag_or_html = tag_or_html.to_s.send(options[:method]) if tag_or_html.is_a?(Tag)
     options[:class] = "label tag-label #{options[:class]}".strip
     if options.delete(:active)
       options[:class] << " label-success"
