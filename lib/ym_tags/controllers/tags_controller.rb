8641962db426e5ff4086852d57b98c8dfe0d2ee5
Matt Atkins <matt@yoomee.com>
2012-08-16 10:18:52 +0100

    Prevent LocalJumpError

--- lib/ym_tags/controllers/tags_controller.rb
+++ lib/ym_tags/controllers/tags_controller.rb
@@ -2,11 +2,11 @@ module YmTags::TagsController
   
   def self.included(base)
     base.expose(:current_tag) {
-      if tag = Tag.find_by_name(params[:id])
-        return tag
-      else
+      tag = Tag.find_by_name(params[:id])
+      if tag.nil?
         raise ActiveRecord::RecordNotFound.new("Couldn't find Tag with name=#{params[:id]}")
       end
+      tag
     }
     base.expose(:top_tags) {Tag.most_used}
     base.autocomplete :tag, :name, :full => true
