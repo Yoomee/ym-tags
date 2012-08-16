5f51b3960a646effb457ff9532ade7e95a66dee0
Edward Andrews <edward@yoomee.com>
2012-08-16 13:22:53 +0100

    Make a formtastic tag_list input, version to 0.1.5

--- lib/ym_tags/models/tag.rb
+++ lib/ym_tags/models/tag.rb
@@ -6,6 +6,15 @@ module YmTags::Tag
   def self.included(base)
     base.scope :contexts, lambda {|contexts| base.joins(:taggings).where(["taggings.context IN (?)", [*contexts]]).group('tags.id')}
     base.scope :most_used, base.joins(:taggings).select("tags.*, COUNT(taggings.id) as tag_count").order('tag_count DESC').group('tags.id')
+    base.send(:extend, ClassMethods)
+  end
+  
+  module ClassMethods
+    def tag_list_options(contexts, resource, tags = nil)
+      tags ||= self.contexts(contexts)
+      resource_tags = [*contexts].collect {|context| resource.send("#{context.to_s.singularize}_list")}
+      (tags.collect(&:name) + resource_tags.flatten).uniq.sort
+    end
   end
   
   def to_s
