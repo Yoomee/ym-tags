0bcfd584cd6e41e6853dd8e15be226d65b5cbebb
Matt Atkins <matt@yoomee.com>
2012-08-16 09:56:02 +0100

    Raise ActiveRecord::RecordNotFound if tag doesn't exist

--- lib/ym_tags/controllers/tags_controller.rb
+++ lib/ym_tags/controllers/tags_controller.rb
@@ -1,7 +1,13 @@
 module YmTags::TagsController
   
   def self.included(base)
-    base.expose(:current_tag) {Tag.find_by_name(params[:id])}
+    base.expose(:current_tag) {
+      if tag = Tag.find_by_name(params[:id])
+        return tag
+      else
+        raise ActiveRecord::RecordNotFound.new("Couldn't find Tag with name=#{params[:id]}")
+      end
+    }
     base.expose(:top_tags) {Tag.most_used}
     base.autocomplete :tag, :name, :full => true
   end
