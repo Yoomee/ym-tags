9819809806b0bdd60389758fd82d6ecfd674a0ef
Ian Mooney <ian@yoomee.com>
2013-08-08 11:26:43 +0100

    Remove decent_exposure references

--- lib/ym_tags/controllers/tags_controller.rb
+++ lib/ym_tags/controllers/tags_controller.rb
@@ -1,15 +1,14 @@
 module YmTags::TagsController
   
   def self.included(base)
-    base.expose(:current_tag) {
-      tag = Tag.find_by_name(params[:id])
-      if tag.nil?
-        raise ActiveRecord::RecordNotFound.new("Couldn't find Tag with name=#{params[:id]}")
-      end
-      tag
-    }
-    base.expose(:top_tags) {Tag.most_used}
     base.autocomplete :tag, :name, :full => true
+  end
+  
+  def show
+    @tag = Tag.find_by_name(params[:id])
+    if @tag.nil?
+      raise ActiveRecord::RecordNotFound.new("Couldn't find Tag with name=#{params[:id]}")
+    end    
   end
   
 end
